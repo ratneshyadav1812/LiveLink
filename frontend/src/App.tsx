4d031ec7918d32af7b9cebc56544e865ad509974
Ratnesh Yadav <ratneshyadav1812@gmail.com>
2025-06-03 22:02:57 +0530

    Add the navabar component over all the pages s

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import { Routes, Route } from "react-router-dom"
 import { Home } from "./pages/Home/Home"
+import { Navbar } from "./components/shared/Navigation"
 function App() {
 
   return (
     <>
+    <Navbar></Navbar>
       <Routes>
         <Route element={<Home />} path="/" />
         <Route element={<Home />} path="/" />
