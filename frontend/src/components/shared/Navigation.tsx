018cab268c03ffdeb427512ca0678396227b176b
Ratnesh Yadav <ratneshyadav1812@gmail.com>
2025-06-04 00:19:29 +0530

    Create the navbar component of the website

--- frontend/src/components/shared/Navigation.tsx
+++ frontend/src/components/shared/Navigation.tsx
@@ -1,5 +1,15 @@
+import { Link } from "react-router-dom";
+
 export function Navbar() {
     return <>
-        This is navbar
-    </>
+        <nav>
+            <div className="mt-[51px] ml-[61px] ">
+                <Link className="flex flex-row" to={'/'}>
+                    <img className="w-[55.3px] h-[50px]" src="/logo.png" alt="logo" /><span className="py-1 ml-2 text-[30px]">GETHORY</span>
+                </Link>
+            </div>
+            
+
+        </nav>    
+        </>
 }
