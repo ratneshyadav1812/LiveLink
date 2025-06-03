809da0e2e0f9329826df93b4837f86162d0bf706
Ratnesh Yadav <ratneshyadav1812@gmail.com>
2025-06-04 02:18:30 +0530

    Create the signin page with the light bg

---
+++ frontend/src/pages/Signin/Signin.tsx
@@ -0,0 +1,10 @@
+import { DarkLogo } from "../../components/shared/logo";
+import { ProfileIcon } from "../../components/shared/profile";
+import './Signin.module.css'
+export function Signin() {
+    return <>
+        <div className="flex flex-row justify-between w-100%">
+            <DarkLogo></DarkLogo>
+        </div>
+    </>
+}
