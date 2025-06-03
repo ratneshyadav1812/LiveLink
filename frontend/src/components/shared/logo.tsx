f2402619334ea117d9b8c2059e8073c1bdef47e2
Ratnesh Yadav <ratneshyadav1812@gmail.com>
2025-06-04 02:17:31 +0530

    Add one more logo apt for the light background pages

--- frontend/src/components/shared/logo.tsx
+++ frontend/src/components/shared/logo.tsx
@@ -2,7 +2,15 @@ import { Link } from "react-router-dom"
 export const Logo = () => {
     return <>
         <Link className="flex flex-row" to={'/'}>
-            <img className="w-[55.3px] h-[50px]" src="/logo.png" alt="logo" /><span className="py-1 ml-2 text-[30px]">GETHORY</span>
+            <img className="w-[36.87px] h-[33px]" src="/logo.png" alt="logo" /><span className="py-1 ml-2 text-[20px] ">GETHORY</span>
+        </Link>
+    </>
+}
+
+export const DarkLogo = ()=>{
+    return <>
+    <Link className="flex flex-row" to={'/'}>
+            <img className="w-[36.87px] h-[33px]" src="/darklogo.png" alt="logo" /><span className="pt-0.5 ml-0.3 text-[20px] text-[#1A202C]">GETHORY</span>
         </Link>
     </>
 }
