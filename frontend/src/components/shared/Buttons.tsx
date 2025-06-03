f4ac82c1749ad1c634959c69b524d3fe2f303bb5
Ratnesh Yadav <ratneshyadav1812@gmail.com>
2025-06-04 02:28:45 +0530

    Add other Dark colored custom button ui

--- frontend/src/components/shared/Buttons.tsx
+++ frontend/src/components/shared/Buttons.tsx
@@ -3,11 +3,20 @@ interface ButtonProps {
     name: string
 
 }
+export function DarkButton(
+    props: ButtonProps
+) {
+    return <>
+        <button className="bg-[#16A394] border-[#71E8DF] border-[3px] px-[20px] w-fit rounded-lg" onClick={props.onclick}>
+            {props.name}
+        </button>
+    </>
+}
 export function LightButton(
     props: ButtonProps
 ) {
     return <>
-        <button className="bg-[#16A394] border-[#71E8DF] border-[3px] w-fit rounded-md" onClick={props.onclick}>
+        <button className="text-[#71E8DF] border-[#FFFFFF] border-[3px] px-[20px] w-fit rounded-lg" onClick={props.onclick}>
             {props.name}
         </button>
     </>
