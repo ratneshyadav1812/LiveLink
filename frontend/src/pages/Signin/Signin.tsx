64cba7dd8fc530a2cf5545668bc0d297da50ab39
Ratnesh Yadav <ratneshyadav1812@gmail.com>
2025-06-04 03:03:02 +0530

    Add one more button to the Signin box to login thorugh google

--- frontend/src/pages/Signin/Signin.tsx
+++ frontend/src/pages/Signin/Signin.tsx
@@ -9,7 +9,8 @@ export function Signin() {
         <div className="ml-[40px] my-2 w-screen h-screen flex flex-col" >
             <div className="text-[33.3px] text-[#1A202C]">WELCOME BACK</div>
             <div className="text-[16px] text-[#4A5568]">Log in to your account to continue</div>
-            <SigninButton name="GitHub" path="/GitHub.png" onclick={() => { }} />
+            <SigninButton name="GitHub" path="/github.png" onclick={() => { }} />
+            <SigninButton name="Google" path="/google.png" onclick={() => { }} />
         </div>
     </>
 }
