531fdd05ff2e129bc36c1804085cbaec2286f548
Ratnesh Yadav <ratneshyadav1812@gmail.com>
2025-06-04 23:09:33 +0530

    Create a Dark bg title card component

--- frontend/src/components/shared/TitleCard.tsx
+++ frontend/src/components/shared/TitleCard.tsx
@@ -12,3 +12,13 @@ export function TitleCard(props: TitleCardProps) {
         </div>
         </>
 }
+export function LighttitleCard(props: TitleCardProps) {
+    return <>
+        <div className="text-[33.3px] h-[35px] text-[30px] mt-[5%]">
+            {props.title}
+        </div>
+        <div className="text-[16px] text-[#9E9E9E] mt-0">
+            {props.subtitle}
+        </div>
+        </>
+}
