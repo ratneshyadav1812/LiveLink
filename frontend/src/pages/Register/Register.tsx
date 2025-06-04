a12114c16aaca5bef4ccdaf8a07348de9a3f9fce
Ratnesh Yadav <ratneshyadav1812@gmail.com>
2025-06-04 17:34:04 +0530

    Similarly create the Register page for the wwebsite

---
+++ frontend/src/pages/Register/Register.tsx
@@ -0,0 +1,43 @@
+import { Link } from "react-router-dom";
+import { LoginSubmitButton, SigninButton } from "../../components/shared/Buttons";
+import { SigninInput } from "../../components/shared/Input";
+import { LightNavbar } from "../../components/shared/Navigation";
+
+import './Register.module.css'
+
+export function Register() {
+    return <>
+        <LightNavbar />
+        <div className="ml-[40px] my-2 w-screen h-screen flex flex-col" >
+            <div className="text-[33.3px] h-[35px] text-[#1A202C]">WELCOME BACK</div>
+            <div className="text-[16px] text-[#4A5568] mt-0">Create your account here!</div>
+            <div className="my-3"><SigninButton name="GitHub" path="/github.png" onclick={() => { }} />
+            </div>
+
+            <SigninButton name="Google" path="/google.png" onclick={() => { }} />
+
+            <div className="w-[80%]  flex flex-row mt-2 items-center">
+                <div className="w-[44%] h-[1px] border border-black">
+
+                </div>
+                <span className="mx-1 text-[16px] text-[#A0AEC0]"> or </span>
+                <div className="w-[44%] h-[1px] border border-black">
+
+                </div>
+            </div>
+            <form action="" className="flex flex-col justify-between h-[230px]">
+                <SigninInput label="Phone Number" id="101" placeholder="Enter your number" />
+                <SigninInput label="Password" id="102" placeholder="Enter your password" />
+                <LoginSubmitButton name="Login" onclick={() => { }} />
+            </form>
+            <div>
+
+            </div>
+            <div className="flex flex-row w-[75%] justify-center my-2 items-center">
+                <span className="text-[#4A5568] text-[16px] ">Already have an account?</span>
+                <Link to={'/signin'}className="text-[#4A5568] text-[16px] underline mx-1"> Log in</Link>
+            </div>
+        </div>
+
+    </>
+}
