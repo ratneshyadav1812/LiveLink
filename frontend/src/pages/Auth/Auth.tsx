12d370c5463eeeafdb4779d7171ec157d8f5d5db
Ratnesh Yadav <ratneshyadav1812@gmail.com>
2025-06-05 00:04:26 +0530

    Add the card showing loading animation on the Screen while user is being set up

--- frontend/src/pages/Auth/Auth.tsx
+++ frontend/src/pages/Auth/Auth.tsx
@@ -1,5 +1,5 @@
 
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { DarkButton } from "../../components/shared/Buttons";
 import { DarkInput } from "../../components/shared/Input";
 import { Navbar } from "../../components/shared/Navigation";
@@ -19,7 +19,7 @@ export function Auth() {
             content = <Step3Card setStep={setStep} />;
             break;
         case 3:
-            content = <Step4Card setStep={setStep} />;
+            content = <Step4Card setStep={setStep} limit={3000} />;
             break;
         default:
             content = null;
@@ -91,3 +91,22 @@ export function Step3Card({ setStep }: { setStep: React.Dispatch<React.SetStateA
         </div>
     </>
 }
+
+
+export function Step4Card({ setStep, limit }: { setStep: React.Dispatch<React.SetStateAction<number>>, limit: number }) {
+    
+    return <>
+        <div className="relative w-full h-full flex flex-col items-center justify-center ">
+            <div className="w-[80.67%] h-[75%] bg-[#0B1D23] rounded-2xl mx-auto absolute inset-0 z-0 blur-lg">
+            </div>
+            <div className="absolute inset-0 z-10 w-[80.67%] h-[50%] mx-auto flex flex-col items-center justify-between">
+                <LighttitleCard title="Seting up your Gethory account" />
+                <div className="flex flex-col items-center w-full">
+                    <div className="w-[23vh] h-[23vh] border-r-[#D9D9D9] animate-spin border-[2vh] border-[#71E8DF] bg-transparent rounded-full"></div>
+                </div>
+
+            </div>
+        </div>
+    </>
+}
+
