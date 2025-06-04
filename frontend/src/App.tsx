4c1c5fdebdf1f0e37596068e7a69609f76539325
Ratnesh Yadav <ratneshyadav1812@gmail.com>
2025-06-04 19:12:28 +0530

    Add the path to submit the otp for validation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom"
 import { Home } from "./pages/Home/Home"
 import { Signin } from "./pages/Signin/Signin"
 import { Register } from "./pages/Register/Register"
+import { ValidateOTP } from "./pages/ValidateOTP/Validation"
 function App() {
 
   return (
@@ -11,6 +12,7 @@ function App() {
           <Route element={<Home />} path="/" />
           <Route element={<Signin />} path="/signin" />
           <Route element={<Register />} path="/register" />
+          <Route element={<ValidateOTP />} path="/submitotp" />
 
       </Routes>
 
