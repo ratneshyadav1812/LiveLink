f5792289b68a9f845e2ad8dae9ef3e309e2a72a8
Ratnesh Yadav <ratneshyadav1812@gmail.com>
2025-06-05 17:16:10 +0530

    Add the Menu card for profile menus

--- frontend/src/components/shared/TitleCard.tsx
+++ frontend/src/components/shared/TitleCard.tsx
@@ -1,8 +1,8 @@
-interface TitleCardProps {
+interface CardProps {
     title: string,
     subtitle?: string
 }
-export function TitleCard(props: TitleCardProps) {
+export function TitleCard(props: CardProps) {
     return <>
         <div className="text-[33.3px] h-[35px] text-[#1A202C]">
             {props.title}
@@ -10,9 +10,9 @@ export function TitleCard(props: TitleCardProps) {
         <div className="text-[16px] text-[#4A5568] mt-0">
             {props.subtitle}
         </div>
-        </>
+    </>
 }
-export function LighttitleCard(props: TitleCardProps) {
+export function LighttitleCard(props: CardProps) {
     return <>
         <div className="text-[33.3px] h-[35px] text-[30px] mt-[5%]">
             {props.title}
@@ -20,5 +20,13 @@ export function LighttitleCard(props: TitleCardProps) {
         <div className="text-[16px] text-[#9E9E9E] mt-0">
             {props.subtitle}
         </div>
-        </>
+    </>
+}
+
+export function MenuCard(props: CardProps) {
+    return <>
+        <div className="text-white text-[20px] pb-3 pr-2 h-[35px] border-b-[3px] border-[#71E8DF] w-max ">
+            {props.title}
+        </div>
+    </>
 }
