f7092e5dbb19042bcf3e4094917fa4b3bc2d9683
Ratnesh Yadav <ratneshyadav1812@gmail.com>
2025-06-05 18:25:19 +0530

    Show the follower and following count on the webpage

--- frontend/src/pages/Profile/Profile.tsx
+++ frontend/src/pages/Profile/Profile.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './Profile.module.css'
 import { NavbarWithProfile } from '../../components/shared/Navigation'
 import { MenuCard } from '../../components/shared/Card'
-import { ProfileBox } from '../../components/shared/profile'
+import { FollowersBox, ProfileBox } from '../../components/shared/profile'
 export function Profile() {
     const [name, setName] = useState("Harshit")
 
@@ -13,8 +13,9 @@ export function Profile() {
                 <MenuCard title='Profile' /></div>
             <div className='relative h-[80%] w-full '>
                 <div className='absolute inset-0 opacity-75 z-0  h-[100%] w-full bg-[#0B1D23]'></div>
-                <div className='absolute text-white z-10 w-full flex flex-row justify-between'>
-                <ProfileBox name='Harshit Rai' username='rrai21' followers={15} following={12}/>
+                <div className='absolute text-white z-10 w-full grid grid-cols-1 md:grid-cols-2 justify-between p-4'>
+                    <ProfileBox name='Harshit Rai' username='rrai21' followers={15} following={12} />
+                    <FollowersBox followers={12} following={33} />
                 </div>
             </div>
         </div>
