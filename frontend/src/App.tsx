1ba26f0378b7672b0106aa073c79d34463ae8fa5
Ratnesh Yadav <ratneshyadav1812@gmail.com>
2025-06-05 17:19:14 +0530

    Add the paths of profile and validate otp to the App tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,8 @@ import { Home } from "./pages/Home/Home"
 import { Signin } from "./pages/Signin/Signin"
 import { Register } from "./pages/Register/Register"
 import { ValidateOTP } from "./pages/ValidateOTP/Validation"
+import { Auth } from "./pages/Auth/Auth"
+import { Profile } from "./pages/Profile/Profile"
 function App() {
 
   return (
@@ -13,6 +15,8 @@ function App() {
           <Route element={<Signin />} path="/signin" />
           <Route element={<Register />} path="/register" />
           <Route element={<ValidateOTP />} path="/submitotp" />
+          <Route element={<Auth />} path="/auth" />
+          <Route element={<Profile />} path="/profile" />
 
       </Routes>
 
