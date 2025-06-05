b0177c9a2a1511027375cd11ea02acbc68f76f48
Ratnesh Yadav <ratneshyadav1812@gmail.com>
2025-06-05 17:12:21 +0530

    Modify and correct the styling for the Profile icons

--- frontend/src/components/shared/profile.tsx
+++ frontend/src/components/shared/profile.tsx
@@ -1,9 +1,12 @@
 export const ProfileIcon = ({ name }: { name: string }) => {
     return <>
-        <div className="flex flex-row">
-            <span className="text-[20px] text-[#1A202C]">Hello {name}</span>
-            <img className="w-[45.33px]" src="/profile.png" alt="profile" />
+        <div className="flex flex-row items-center ">
+            <span className="text-[20px] text-[#1A202C] text-white mr-2">
+                Hello {name}
+            </span>
+            <div className="w-[45px] h-[45px] rounded-full bg-white flex flex-row justify-center items-center"><img className="w-[45px]" src="/profile.png" alt="profile" /></div>
 
         </div>
     </>
 }
+
