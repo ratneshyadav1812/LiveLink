fc23d3fde4730d8bd72440b33d86080be19f7306
Ratnesh Yadav <ratneshyadav1812@gmail.com>
2025-06-05 17:11:38 +0530

    Add the Dark navbar with profile icons component

--- frontend/src/components/shared/Navigation.tsx
+++ frontend/src/components/shared/Navigation.tsx
@@ -1,10 +1,22 @@
 import { DarkLogo, Logo } from "./logo";
+import { ProfileIcon } from "./profile";
 
 export function Navbar() {
     return <>
         <nav>
             <div className="mt-[33.33px] ml-[40px] ">
                 <Logo />
+            </div>
+        </nav>
+    </>
+}
+export function NavbarWithProfile() {
+    return <>
+        <nav className="w-full px-[40px] border-b border-[#A0AEC0] pb-3 border-b-[1px]">
+            <div className="flex flex-row w-full mt-[33.33px] justify-between ">
+                <Logo />
+                <ProfileIcon name="Harshit" />
+
             </div>
         </nav>
     </>
