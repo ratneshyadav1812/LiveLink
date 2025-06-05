9d4812a6c821919c428b9d5942f295173c62c2e0
Ratnesh Yadav <ratneshyadav1812@gmail.com>
2025-06-05 17:51:06 +0530

    Show the profile box on the webPage

--- frontend/src/pages/Profile/Profile.tsx
+++ frontend/src/pages/Profile/Profile.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './Profile.module.css'
 import { NavbarWithProfile } from '../../components/shared/Navigation'
 import { MenuCard } from '../../components/shared/TitleCard'
+import { ProfileBox } from '../../components/shared/profile'
 export function Profile() {
     const [name, setName] = useState("Harshit")
 
@@ -12,8 +13,8 @@ export function Profile() {
                 <MenuCard title='Profile' /></div>
             <div className='relative h-[80%] w-full '>
                 <div className='absolute inset-0 opacity-75 z-0  h-[100%] w-full bg-[#0B1D23]'></div>
-                <div className='absolute text-white z-10 flex flex-row justify-between'>
-                    
+                <div className='absolute text-white z-10 w-full flex flex-row justify-between'>
+                <ProfileBox name='Harshit Rai' username='rrai21' followers={15} following={12}/>
                 </div>
             </div>
         </div>
