aa4c9747d6bd1c2fd6ef4b361ffaed84a861907c
Ratnesh Yadav <ratneshyadav1812@gmail.com>
2025-06-11 14:05:43 +0530

    Add the section to teh profile tsx

--- frontend/src/pages/Profile/Profile.tsx
+++ frontend/src/pages/Profile/Profile.tsx
@@ -1,24 +1,17 @@
 import { useState } from 'react'
 import './Profile.module.css'
-import { NavbarWithProfile } from '../../components/shared/Navigation'
-import { MenuCard } from '../../components/shared/Card'
 import { FollowersBox, ProfileBox, UserDescBox } from '../../components/shared/UserUI'
+import Section from '../../components/shared/Section'
 export function Profile() {
     const [name] = useState("Harshit")
 
     return <>
-        <NavbarWithProfile />
-        <div className='justify-between flex flex-col w-full h-[90%]'>
-            <div className='m-5'>
-                <MenuCard title='Profile' /></div>
-            <div className='relative h-[80%] w-full '>
-                <div className='absolute inset-0 opacity-75 z-0  h-[100%] w-full bg-[#0B1D23]'></div>
-                <div className='absolute text-white z-10 w-full grid grid-cols-1 md:grid-cols-2 justify-between p-4'>
-                    <ProfileBox name={name} username='rrai21' followers={15} following={12} />
-                    <FollowersBox followers={12} following={33} />
-                    <UserDescBox desc='Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium cumque dignissimos architecto minima ea adipisci ex sequi dolorum. Consectetur, vitae.' />
-                </div>
+        <Section>
+            <div className='absolute text-white z-10 w-full grid grid-cols-1 md:grid-cols-2 justify-between p-4'>
+                <ProfileBox name={name} username='rrai21' followers={15} following={12} />
+                <FollowersBox followers={12} following={33} />
+                <UserDescBox desc='Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium cumque dignissimos architecto minima ea adipisci ex sequi dolorum. Consectetur, vitae.' />
             </div>
-        </div>
+        </Section>
     </>
 }
