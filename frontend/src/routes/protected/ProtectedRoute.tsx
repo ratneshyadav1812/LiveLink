47f82f0e763dc725aa013676c9ffa9b27f7bf48a
Ratnesh Yadav <ratneshyadav1812@gmail.com>
2025-06-11 13:37:46 +0530

    Create a user object and render diff components on the screen according to the user auth status

--- frontend/src/routes/protected/ProtectedRoute.tsx
+++ frontend/src/routes/protected/ProtectedRoute.tsx
@@ -3,12 +3,19 @@ import { Navigate } from "react-router-dom"
 interface RouteProps {
     children: ReactNode
 }
-const isAuth = false;
+export const user = {
+    authenticated: true,
+    activated: false
+};
 const ProtectedRoute = (props: RouteProps) => {
-    if (!isAuth)
-        return <Navigate to={'/signin'} />
-    else return props.children
+    let component: ReactNode = null;
+    if (!user.authenticated)
+        component = <Navigate to={'/signin'} />
+    else if (!user.activated)
+        component = <Navigate to={'/auth'} />
+    else component = props.children
 
+    return component
 }
 
-export default ProtectedRoute
+export default ProtectedRoute   
