5f4c1c972bcc23bd5e010e0f308b259daed02e09
Ratnesh Yadav <ratneshyadav1812@gmail.com>
2025-06-11 12:09:44 +0530

    Use the protected route to secure the rooms route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,18 +5,21 @@ import { Register } from "./pages/Register/Register"
 import { ValidateOTP } from "./pages/ValidateOTP/Validation"
 import { Auth } from "./pages/Auth/Auth"
 import { Profile } from "./pages/Profile/Profile"
+import ProtectedRoute from "./routes/protected/ProtectedRoute.tsx"
 function App() {
 
   return (
     <>
       <Routes>
 
-          <Route element={<Home />} path="/" />
-          <Route element={<Signin />} path="/signin" />
-          <Route element={<Register />} path="/register" />
-          <Route element={<ValidateOTP />} path="/submitotp" />
-          <Route element={<Auth />} path="/auth" />
-          <Route element={<Profile />} path="/profile" />
+        <Route element={<Home />} path="/" />
+        <Route element={<Signin />} path="/signin" />
+        <Route element={<Register />} path="/register" />
+        <Route element={<ValidateOTP />} path="/submitotp" />
+        <Route element={<Auth />} path="/auth" />
+        <Route element={<Profile />} path="/profile" />
+        <Route path="/rooms" element={<ProtectedRoute><div>Hello You r not auths</div> </ProtectedRoute>}>
+        </Route>
 
       </Routes>
 
