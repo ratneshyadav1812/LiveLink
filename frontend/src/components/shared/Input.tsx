69ac324bd13a9aa31df364e1e47738fb2affad34
Ratnesh Yadav <ratneshyadav1812@gmail.com>
2025-06-14 18:58:52 +0530

    Create deparate for the UI input component

--- frontend/src/components/shared/Input.tsx
+++ frontend/src/components/shared/Input.tsx
@@ -13,13 +13,18 @@ export function SigninInput(props: InputProps) {
     </>
 
 }
-export function DarkInput(props : InputProps ){
+export function DarkInput(props: InputProps) {
     return <>
-    <input className="text-white border border-white w-[10rem] text-center mt-5 bg-transparent px-3 rounded-lg border-[3px]" type="text" name={props.label} id={props.id} placeholder={props.placeholder}/>
+        <input className="text-white border border-white w-[10rem] text-center mt-5 bg-transparent px-3 rounded-lg border-[3px]" type="text" name={props.label} id={props.id} placeholder={props.placeholder} />
     </>
 }
-export function OtpInput({id} : {id : string}) {
+export function OtpInput({ id }: { id: string }) {
     return <>
-                <input type="text" id={id} className='w-[6vw] lg:w-[5vw] lg:h-[5vw] lg:rounded-lg text-black text-[20px] lg:text-[30px]  bg-[#D9D9D9] rounded-md border border-[#4A5568] text-center  shadow-lg'></input>
-            </>
+        <input type="text" id={id} className='w-[6vw] lg:w-[5vw] lg:h-[5vw] lg:rounded-lg text-black text-[20px] lg:text-[30px]  bg-[#D9D9D9] rounded-md border border-[#4A5568] text-center  shadow-lg'></input>
+    </>
+}
+export function GreyInput() {
+    return <>
+        <input type="text" className='bg-[#414A5B] opacity-60 rounded-lg p-1' />
+    </>
 }
