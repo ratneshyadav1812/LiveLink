1511ae1e4d35077f078b2b1e9389659772802619
Ratnesh Yadav <ratneshyadav1812@gmail.com>
2025-09-30 17:22:00 +0530

    Create the component of navbar with logout Button and also makuse of user RTK object to display name

--- frontend/src/components/shared/Navigation.tsx
+++ frontend/src/components/shared/Navigation.tsx
@@ -1,5 +1,8 @@
+import { useSelector } from "react-redux";
 import { DarkLogo, Logo } from "./logo";
+import LogoutButton from "./Logout";
 import { ProfileIcon } from "./UserUI";
+import { selectUser } from "../../store/authSelectors";
 
 export function Navbar() {
     return <>
@@ -11,11 +14,24 @@ export function Navbar() {
     </>
 }
 export function NavbarWithProfile() {
+    const user = useSelector(selectUser)
+    const localName = user?.name 
     return <>
         <nav className="w-full px-[40px] border-b border-[#A0AEC0] pb-3 border-b-[1px]">
             <div className="flex flex-row w-full mt-[33.33px] justify-between ">
                 <Logo />
-                <ProfileIcon name="Harshit" />
+                <ProfileIcon name={localName ?? "NotLogged_User"} />
+
+            </div>
+        </nav>
+    </>
+}
+export function NavbarWithLogout() {
+    return <>
+        <nav className="w-full px-[40px] border-b border-[#A0AEC0] pb-3 border-b-[1px]">
+            <div className="flex flex-row w-full mt-[33.33px] justify-between ">
+                <Logo />
+                <LogoutButton />
 
             </div>
         </nav>
