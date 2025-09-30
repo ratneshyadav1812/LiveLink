0b3e9a0b91f27783bf84751982a4cece7f6eed5a
Ratnesh Yadav <ratneshyadav1812@gmail.com>
2025-09-30 14:43:40 +0530

    Refactor the Semiprotected routes to redirect topt he authCompletion once the user is found to be unverified int eh RTK user store

--- frontend/src/routes/protected/SemiProtected.tsx
+++ frontend/src/routes/protected/SemiProtected.tsx
@@ -1,7 +1,8 @@
 import type { ReactNode } from "react"
 import { Navigate } from "react-router-dom"
 import { useSelector } from 'react-redux';
-import { selectIsAuthenticated, selectNeedsProfileSetup, selectAuthLoading, selectLandingPath } from '../../store/authSelectors'; 
+import { selectIsAuthenticated, selectNeedsProfileSetup, selectAuthLoading,  selectUser } from '../../store/authSelectors'; 
+import AccountPendingPanel from "../../pages/Authcompletion/Authcompletion";
 
 interface RouteProps {
     children: ReactNode
@@ -11,25 +12,33 @@ const SemiProtected = (props: RouteProps)  => {
     const isAuthenticated = useSelector(selectIsAuthenticated);
     const needsSetup = useSelector(selectNeedsProfileSetup);
     const isLoading = useSelector(selectAuthLoading);
-    const landingPath = useSelector(selectLandingPath); 
+    const user  = useSelector(selectUser)
+    // <AccountPendingPanel
 
     // 1. Wait for session check
     if (isLoading) {
         return null;
     }
     
-    // 2. Scenario 1: User is Logged OUT (Allow Children to Render, e.g., Signin/Register)
-    if (!isAuthenticated) {
-        return props.children; 
+   if (isAuthenticated) {
+        // A. Profile is NOT Verified: 
+        if (user && !user.verified) { 
+            // Only render the warning panel on Guest pages (/signin, /register, /home)
+            // We return the custom panel instead of Navigating.
+            return <AccountPendingPanel />;
+        }
+        
+        // B. Profile is Verified but Incomplete: Send to /auth
+        if (needsSetup) {
+            return <Navigate to={'/auth'} replace/>
+        }
+        
+        // C. Fully Complete: Send to /profile
+        return <Navigate to={'/profile'} replace/>
     }
     
-    // 3. Scenario 2: User is Logged IN
-    // If user is logged in, redirect them to their primary destination.
-    if (isAuthenticated) {
-        return <Navigate to={landingPath} replace/>
-    }
-
-    return props.children; 
+    // 3. Scenario: Logged Out (Allow access to Signin/Register forms)
+    return props.children
 }
 
 export default SemiProtected
