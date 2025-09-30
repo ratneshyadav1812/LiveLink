d0aa502b0458c34998a24128a7bbb095e33d4d51
Ratnesh Yadav <ratneshyadav1812@gmail.com>
2025-09-30 18:25:05 +0530

    Use RTK user object to render correct state values

--- frontend/src/pages/Profile/Profile.tsx
+++ frontend/src/pages/Profile/Profile.tsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
 import './Profile.module.css'
 import { FollowersBox, ProfileBox, UserDescBox } from '../../components/shared/UserUI'
 import Section from '../../components/shared/Section'
+import { useSelector } from 'react-redux'
+import { selectUser } from '../../store/authSelectors'
 export function Profile() {
-    const [name] = useState("Harshit")
-
+    const user = useSelector(selectUser)
     return <>
         <Section>
-            <div className='absolute text-white z-10 w-full grid grid-cols-1 md:grid-cols-2 justify-between p-4'>
-                <ProfileBox name={name} username='rrai21' followers={15} following={12} />
+            <div className='absolute text-white z-10 w-full grid grid-cols-1 md:grid-cols-2 justify-between p-4 mt-5' >
+                <ProfileBox name={user?.name ?? 'LoggedOut_User'} username={user?.username || "user_logOut"} followers={15} following={12} />
                 <FollowersBox followers={12} following={33} />
                 <UserDescBox desc='Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium cumque dignissimos architecto minima ea adipisci ex sequi dolorum. Consectetur, vitae.' />
             </div>
