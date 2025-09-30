d408c50d260d2fb5e3bbb81eb8d3c2d6df4e9574
Ratnesh Yadav <ratneshyadav1812@gmail.com>
2025-09-30 21:57:07 +0530

    Change route guards for SemiProtected to not redirect to /verify-status when the location is submitotp

--- frontend/src/routes/protected/SemiProtected.tsx
+++ frontend/src/routes/protected/SemiProtected.tsx
@@ -1,39 +1,43 @@
 import type { ReactNode } from "react"
 import { Navigate } from "react-router-dom"
 import { useSelector } from 'react-redux';
-import { selectIsAuthenticated, selectNeedsProfileSetup, selectAuthLoading,  selectUser } from '../../store/authSelectors'; 
+import { selectIsAuthenticated, selectNeedsProfileSetup, selectAuthLoading, selectUser } from '../../store/authSelectors';
 
 interface RouteProps {
     children: ReactNode
 }
 
-const SemiProtected = (props: RouteProps)  => {
+const SemiProtected = (props: RouteProps) => {
     const isAuthenticated = useSelector(selectIsAuthenticated);
     const needsSetup = useSelector(selectNeedsProfileSetup);
     const isLoading = useSelector(selectAuthLoading);
-    const user  = useSelector(selectUser)
+    const user = useSelector(selectUser)
     // <AccountPendingPanel
 
     // 1. Wait for session check
     if (isLoading) {
         return null;
     }
-    
-   if (isAuthenticated) {
+
+    if (isAuthenticated) {
         // A. Profile is NOT Verified: 
-        if (user && !user.verified) { 
+        if (user && !user.verified) {
+            console.log(window.location.pathname)
+            if (window.location.pathname === '/submitotp') {
+                return <Navigate to={'/submitotp'} replace />
+            }
             return <Navigate to={'/verify-status'} replace />;
         }
-        
+
         // B. Profile is Verified but Incomplete: Send to /auth
         if (needsSetup) {
-            return <Navigate to={'/auth'} replace/>
+            return <Navigate to={'/auth'} replace />
         }
-        
+
         // C. Fully Complete: Send to /profile
-        return <Navigate to={'/profile'} replace/>
+        return <Navigate to={'/profile'} replace />
     }
-    
+
     // 3. Scenario: Logged Out (Allow access to Signin/Register forms)
     return props.children
 }
