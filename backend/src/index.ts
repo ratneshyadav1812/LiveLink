dde7afa7fd01e6248905ae7767727f6501f65352
Ratnesh Yadav <ratneshyadav1812@gmail.com>
2025-10-02 03:26:40 +0530

    Static Serve the image files using the GET route on backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,6 @@
 import "dotenv/config"
 import express from 'express'
+import path from "path";
 import { Request, Response, NextFunction } from "express";
 import cors from 'cors'
 import { DB_URI, PORT, NODE_ENV, APP_ORIGIN } from "./constants/env";
@@ -18,8 +19,7 @@ app.use(cors({
 }))
 app.use(express.json({ limit: '10mb' })); 
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
-
-app.use(cookieParser())
+app.use("/store", express.static(path.join(__dirname, "/store")));app.use(cookieParser())
 app.get("/error", catchError(async (req: Request, res: Response, next: NextFunction) => {
     console.log("Hello")
     throw new Error("booo");
