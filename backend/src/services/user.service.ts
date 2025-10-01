861cd8c870e329912e820fc9290f5dfd403e26a7
Ratnesh Yadav <ratneshyadav1812@gmail.com>
2025-10-02 03:26:10 +0530

    Using the Jimp module resize the avatar images to lower resolution

--- backend/src/services/user.service.ts
+++ backend/src/services/user.service.ts
@@ -1,19 +1,44 @@
 import { HTTP } from "../constants/http";
 import User from "../models/UserCollection"
 import { appAssert } from "../utils/appAssert";
+import Jimp from 'jimp'
+import path from 'path'
 
-export const updateUser = async (userId: any, username: string, name: string, avatar ?: string) => {
-    console.log('Update Object : ' , userId , username)
-    const updatedUser = await User.findByIdAndUpdate(userId, {
-        $set: {
-            username,
-            name,
-            avatar,
-            profileCompleted: true
-        }
-    }, { new: true })
-    console.log(updatedUser)
+export const updateUser = async (
+    userId: any,
+    username: string,
+    name: string,
+    avatar?: string
+) => {
+    console.log("Update Object : ", userId, username);
+
+    let imgPath: string | undefined;
+
+    if (avatar) {
+        const buffer = Buffer.from(avatar.split(",")[1], "base64");
+        const image = await Jimp.read(buffer);
+
+        image.resize(150, Jimp.AUTO);
+
+        imgPath = `${Date.now()}.${Math.floor(Math.random() * 1e6)}.png`;
+        await image.writeAsync(path.resolve(__dirname, `../storage/${imgPath}`));
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(
+        userId,
+        {
+            $set: {
+                username,
+                name,
+                avatar: imgPath,
+                profileCompleted: true,
+            },
+        },
+        { new: true }
+    );
+
+    console.log(updatedUser);
     appAssert(updatedUser, HTTP.NOT_FOUND, "User not found or update failed");
-    return updatedUser
 
-}
+    return updatedUser;
+};
