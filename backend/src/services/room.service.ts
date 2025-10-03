625408f19e88da6ec5aff076da9e510ba813476c
Ratnesh Yadav <ratneshyadav1812@gmail.com>
2025-10-04 00:35:26 +0530

    Create the service to fetch the rooms from teh mongomodel based on the roomtype

--- backend/src/services/room.service.ts
+++ backend/src/services/room.service.ts
@@ -1,13 +1,23 @@
 import mongoose from "mongoose";
 import RoomModel, { RoomModelDocument } from "../models/RoomModel";
+import { appAssert } from "../utils/appAssert";
+import { HTTP } from "../constants/http";
 
 export const createRoomService = async (payload: RoomModelDocument) => {
     const { topic, roomType, userId } = payload
+
     const room = await RoomModel.create({
         userId,
         topic,
         roomType,
         speakers: [userId]
     })
+    appAssert(room, HTTP.INTERNAL_SERVER_ERROR, "DB error")
     return room
+}
+
+export const getRoomService = async (types: any) => {
+    const rooms = await RoomModel.find({ roomType: { $in: types } }
+    ).populate('speakers').populate('userId').exec()
+    return rooms
 }
