2222d2f702bee9b18ab00b6ca7674d7c65cac533
Ratnesh Yadav <ratneshyadav1812@gmail.com>
2025-10-04 01:42:36 +0530

    Create the XML component New Room and export it

---
+++ frontend/src/pages/NewRoom/Newroom.tsx
@@ -0,0 +1,13 @@
+import { useNavigate } from "react-router-dom"
+import { SectionType2 } from "../../components/shared/Section"
+
+const Newroom = () => {
+    
+  return (
+    <SectionType2>
+        <div>Hello</div>
+    </SectionType2>
+  )
+}
+
+export default Newroom
