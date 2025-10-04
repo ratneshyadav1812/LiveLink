ac6597e8ca43e677444b0ba868bd1268be09fd46
Ratnesh Yadav <ratneshyadav1812@gmail.com>
2025-10-04 16:32:46 +0530

    Create the logic to capture user VoiceStream and push into the existing  clients d setThe locslMediaa Stream using the callback

--- frontend/src/hooks/useWebRTC.tsx
+++ frontend/src/hooks/useWebRTC.tsx
@@ -1,12 +1,17 @@
-import { useRef } from "react";
+import { useCallback, useEffect, useRef } from "react";
 import type { User } from "../store/authSlice";
 import { useStateWithCallback } from "./useStateWithCallback";
-const joineeDummyData = [{
-    _id: 1,
+export type ClientInterface = {
+    _id: string,
+    name: string
+}
+
+const joineeDummyData: ClientInterface[] = [{
+    _id: '1',
     name: "Rakesh K"
 },
 {
-    _id: 2,
+    _id: '2',
     name: "Kaishal Openuy"
 }]
 
@@ -16,12 +21,40 @@ export interface AudioInterface {
 export const useWebRTC = (roomId: string, user: User) => {
     const [clients, setClients] = useStateWithCallback(joineeDummyData);
     const audioElements = useRef<AudioInterface>({})
-    const localmediaStream = useRef(null)
+    const localmediaStream = useRef<MediaStream>(null)
     const connections = useRef({})
 
     const provideRef = (instance: any, userId: number | string) => {
         audioElements.current[userId] = instance
     }
+
+    const addNewClient = useCallback((newClient: ClientInterface, cb: () => any) => {
+        const alreadyPresent = clients.find((client) => client._id === newClient._id)
+
+        if (!alreadyPresent) setClients(
+            (existingClients: ClientInterface[]) => {
+                return [...existingClients, newClient]
+            }, cb)
+
+    }, [clients, setClients])
+
+    //Start capture media
+    useEffect(() => {
+        const startCapture = async () => {
+            localmediaStream.current = await navigator.mediaDevices.getUserMedia({
+                audio: true
+            })
+        }
+        startCapture().then(() => {
+            //Now add to the list of Clients
+            //@ts-ignore 
+            addNewClient(user, () => {
+                const localElement = audioElements.current[user._id]
+                localElement.volume = 0
+                localElement.srcObject = localmediaStream.current
+            })
+        }).catch((err) => { })
+    }, [])
     return { clients, provideRef }
 
 }
