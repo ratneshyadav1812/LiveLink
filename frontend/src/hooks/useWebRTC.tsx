a55558c01df5d2aed71e0e8263030fbd855d4386
Ratnesh Yadav <ratneshyadav1812@gmail.com>
2025-10-04 15:45:23 +0530

    Make the use of audio ref attribute to control every audio element

--- frontend/src/hooks/useWebRTC.tsx
+++ frontend/src/hooks/useWebRTC.tsx
@@ -1,3 +1,5 @@
+import { useRef } from "react";
+import type { User } from "../store/authSlice";
 import { useStateWithCallback } from "./useStateWithCallback";
 const joineeDummyData = [{
     _id: 1,
@@ -7,9 +9,19 @@ const joineeDummyData = [{
     _id: 2,
     name: "Kaishal Openuy"
 }]
-export const useWebRTC = () => {
-    const [clients, setClients] = useStateWithCallback(joineeDummyData);
 
-    return { clients }
+export interface AudioInterface {
+    [userId: string]: any;
+}
+export const useWebRTC = (roomId: string, user: User) => {
+    const [clients, setClients] = useStateWithCallback(joineeDummyData);
+    const audioElements = useRef<AudioInterface>({})
+    const localmediaStream = useRef(null)
+    const connections = useRef({})
+
+    const provideRef = (instance: any, userId: number | string) => {
+        audioElements.current[userId] = instance
+    }
+    return { clients, provideRef }
 
 }
--- frontend/src/pages/NewRoom/Newroom.tsx
+++ frontend/src/pages/NewRoom/Newroom.tsx
@@ -1,13 +1,39 @@
-import { useNavigate } from "react-router-dom"
 import { SectionType2 } from "../../components/shared/Section"
+import { MenuCard } from "../../components/shared/Card"
+import { useWebRTC } from "../../hooks/useWebRTC"
+import { useParams } from "react-router-dom"
+import { useSelector } from "react-redux"
+import { selectUser } from "../../store/authSelectors"
 
 const Newroom = () => {
-    
+
   return (
-    <SectionType2>
-        <div>Hello</div>
-    </SectionType2>
+    <div>
+      <SectionType2>
+        <MenuCard title="All joinees" />
+
+
+      </SectionType2>
+      <Joinee />
+    </div>
   )
 }
-
+const Joinee = () => {
+  const { id: roomId } = useParams()
+  const user = useSelector(selectUser)
+  if (!roomId || !user) return <>Not Verified</>
+  const { clients, provideRef } = useWebRTC(roomId, user)
+  return <>
+    <div>
+      {clients.map((client: any) => {
+        return <>
+          <div>
+            <audio ref={(instance) => provideRef(instance, client.id)} controls autoPlay></audio>
+            <h4>{client.name}</h4>
+          </div>
+        </>
+      })}
+    </div>
+  </>
+}
 export default Newroom
