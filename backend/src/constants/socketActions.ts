09ba05bf112c8a6e38c9bf49e81b32b869742178
Ratnesh Yadav <ratneshyadav1812@gmail.com>
2025-10-04 23:51:09 +0530

    Update the socketActions to match the frontend

--- backend/src/constants/socketActions.ts
+++ backend/src/constants/socketActions.ts
@@ -1,6 +1,7 @@
 export const ACTIONS = {
     LEAVE: 'leave',
     JOIN: 'join',
-    ADD_PEER : 'add-peer'
-
+    ADD_PEER : 'add-peer',
+    RELAY_ICE :  'relay-ice',
+    RELAY_SDP : 'relay-sdp'
 }
