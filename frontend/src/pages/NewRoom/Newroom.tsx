b0a2b8f47c1025bea5cf447f9fb5ed2b15a024c5
Ratnesh Yadav <ratneshyadav1812@gmail.com>
2025-10-06 01:38:12 +0530

    Use the useeffect to call the backend to populate the localState for the room using api call

--- frontend/src/pages/NewRoom/Newroom.tsx
+++ frontend/src/pages/NewRoom/Newroom.tsx
@@ -1,5 +1,6 @@
 import { SectionType2 } from "../../components/shared/Section"
 import { MenuCard } from "../../components/shared/Card"
+import MicOffIcon from '@mui/icons-material/MicOff';
 import ExitToAppIcon from '@mui/icons-material/ExitToApp';
 import FrontHandIcon from '@mui/icons-material/FrontHand';
 import { useWebRTC, type ClientInterface } from "../../hooks/useWebRTC"
@@ -9,12 +10,23 @@ import { selectUser } from "../../store/authSelectors"
 import styles from './Newroom.module.css'
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import { ButtonWithLogo } from "../../components/shared/Buttons";
+import { useEffect, useState } from "react";
 const Newroom = () => {
   const navigate = useNavigate()
+  const [room, setRoom] = useState({})
+  const { id: roomId } = useParams()
   const topicMeet = 'My Meet on Ai'
   const handleManualLeave = () => {
     navigate('/rooms', { replace: true })
   }
+
+  useEffect(() => {
+    const fetchRoomDetails = async () => {
+      const { data } = await getRoomDetails(roomId)
+      setRoom(data.room)
+    }
+    fetchRoomDetails()
+  }, [roomId])
   return (
     <div className="flex flex-col items-center">
       <SectionType2>
@@ -34,27 +46,27 @@ const Newroom = () => {
           </div>
         </div>
         <div>
-          <Joinee />
+          <Joinee roomId={roomId} />
         </div>
       </div>
     </div>
   )
 }
-const Joinee = () => {
-  const { id: roomId } = useParams()
+const Joinee = ({ roomId }: { roomId: string | undefined }) => {
   const user = useSelector(selectUser)
   //@ts-ignore
   const { clients, provideRef } = useWebRTC(roomId, user)
   return <>
 
-    <div className="grid grid-cols-4 md:grid-cols-6 lg:grid-cols-8">
+    <div className="grid grid-cols-4 md:grid-cols-6 lg:grid-cols-8 items-center w-full">
       {clients.map((client: ClientInterface) => {
-        return <div className="w-fit m-3 flex flex-col items-center" key={client._id}>
+        return <div className="w-fit m-3 flex flex-col items-center justify-self-center col-span-1" key={client._id}>
           <div className={styles.userHead}>
             <audio ref={(instance) => provideRef(instance, client._id)} autoPlay></audio>
             <img className={styles.userAvatar}
               //@ts-ignore
               src={client.avatar} alt="" />
+            <button className={styles.micBtn}><MicOffIcon fontSize="inherit" /></button>
           </div>
           <h4>{client.name}</h4>
         </div>
