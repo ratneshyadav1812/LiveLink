47584b001ebcb50a8a86ae006af2744761d49878
Ratnesh Yadav <ratneshyadav1812@gmail.com>
2025-10-06 03:05:25 +0530

    Add the muteClick functionality on the webpage

--- frontend/src/pages/NewRoom/Newroom.tsx
+++ frontend/src/pages/NewRoom/Newroom.tsx
@@ -2,6 +2,7 @@ import { SectionType2 } from "../../components/shared/Section"
 import { MenuCard } from "../../components/shared/Card"
 import MicOffIcon from '@mui/icons-material/MicOff';
 import ExitToAppIcon from '@mui/icons-material/ExitToApp';
+import MicIcon from '@mui/icons-material/Mic';
 import FrontHandIcon from '@mui/icons-material/FrontHand';
 import { useWebRTC, type ClientInterface } from "../../hooks/useWebRTC"
 import { useNavigate, useParams } from "react-router-dom"
@@ -10,20 +11,31 @@ import { selectUser } from "../../store/authSelectors"
 import styles from './Newroom.module.css'
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import { ButtonWithLogo } from "../../components/shared/Buttons";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { getRoomDetails } from "../../http/utils";
+
+export interface RoomInterface {
+  userId: string;
+  // expiresAt: Date;
+  topic: string;
+  roomType: string;
+  createdAt: Date;
+  speakers: string[]
+}
 const Newroom = () => {
   const navigate = useNavigate()
-  const [room, setRoom] = useState({})
+  const [room, setRoom] = useState<RoomInterface | null>(null)
   const { id: roomId } = useParams()
-  const topicMeet = 'My Meet on Ai'
-  const handleManualLeave = () => {
+
+
+  const handleManualLeave = useCallback(() => {
     navigate('/rooms', { replace: true })
-  }
+  }, [navigate])
 
   useEffect(() => {
     const fetchRoomDetails = async () => {
       const { data } = await getRoomDetails(roomId)
+      console.log(data)
       //@ts-ignore
       setRoom((prev) => data.room)
     }
@@ -41,7 +53,7 @@ const Newroom = () => {
       </SectionType2>
       <div className="flex flex-col m-5 space-y-2 w-[95%] h-fit">
         <div className="flex justify-between w-full">
-          <MenuCard title={topicMeet} />
+          <MenuCard title={room?.topic ?? ""} />
           <div className="flex space-x-3">  <ButtonWithLogo name="Raise hand" logo={FrontHandIcon} onclick={handleManualLeave} />
             <ButtonWithLogo name="Leave Quietly" logo={ExitToAppIcon} onclick={handleManualLeave} />
 
@@ -57,7 +69,16 @@ const Newroom = () => {
 const Joinee = ({ roomId }: { roomId: string | undefined }) => {
   const user = useSelector(selectUser)
   //@ts-ignore
-  const { clients, provideRef } = useWebRTC(roomId, user)
+  const { clients, provideRef, handleMute } = useWebRTC(roomId, user)
+  const [isMute, setMute] = useState(true)
+  useEffect(() => {
+    handleMute(isMute, user?._id)
+  }, [isMute])
+
+  const handleMuteClick = useCallback((clientId: string) => {
+    if (clientId !== user?._id) return //be able mute yourself only
+    setMute((prev) => !prev)
+  }, [setMute])
   return <>
 
     <div className="grid grid-cols-4 md:grid-cols-6 lg:grid-cols-8 items-center w-full">
@@ -68,7 +89,7 @@ const Joinee = ({ roomId }: { roomId: string | undefined }) => {
             <img className={styles.userAvatar}
               //@ts-ignore
               src={client.avatar} alt="" />
-            <button className={styles.micBtn}><MicOffIcon fontSize="inherit" /></button>
+            <button onClick={() => { handleMuteClick(client._id) }} className={styles.micBtn}>{client.muted ? <MicOffIcon fontSize="inherit" /> : <MicIcon fontSize="inherit" />}</button>
           </div>
           <h4>{client.name}</h4>
         </div>
