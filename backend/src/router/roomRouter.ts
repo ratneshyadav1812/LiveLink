46f9b73abb78a2ace843d500bf68ae9ec34a7ad1
Ratnesh Yadav <ratneshyadav1812@gmail.com>
2025-10-06 01:46:27 +0530

    Create the backend api to handle fetching of route using the dynamic route

--- backend/src/router/roomRouter.ts
+++ backend/src/router/roomRouter.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { fetchAllRoomController, roomCreateController } from "../controllers/room.controllers";
+import { fetchAllRoomController, getRoomController, roomCreateController } from "../controllers/room.controllers";
 const rt = Router()
 
 rt.post('/create' , roomCreateController)
 
 rt.get('/fetchall' , fetchAllRoomController)
 export {rt as roomRouter}
+
+rt.get('/getroom/:roomId' , getRoomController)
